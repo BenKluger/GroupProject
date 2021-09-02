ff29eb2c9dbace3ff166e693961ae1fb68a63168
BenKluger <bennykluger@gmail.com>
2021-09-02 00:00:34 -0400

    Adding all setup files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const port = 3000;
 const fs = require('fs');
+const {pool} = require('./dbConfig');
 
 const server = http.createServer();
 server.on("listening", () => console.log(`Now Listening on Port ${port}`));
